aa7064d902b34c7672c5ab3592fe7b1a03fcaf4c
ykkallu <yashodharkallu@gmail.com>
2022-03-13 22:57:06 +0530

    test

--- com/clientname/source_data_loading.py
+++ com/clientname/source_data_loading.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
 
     s3_conf = app_conf['s3_conf']
-    datalake_path = 's3a://' + s3_conf['s3_bucket'] + s3_conf['datalake_path']
+    datalake_path = 's3a://' + s3_conf['s3_bucket'] + '/' + s3_conf['datalake_path']
     source_list = app_conf['source_list']
 
     for src in source_list:
